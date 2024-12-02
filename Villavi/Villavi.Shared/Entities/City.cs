a96683754be834e2af1c1391a100a95701788a5f
JEETVP <roberto.villegasrvo@gmail.com>
2024-12-02 17:53:02 -0600

    creacion de modelo de clases

---
+++ Villavi/Villavi.Shared/Entities/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villavi.Shared.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage ="El campo {0] debe tener maximo {1] caracteres")]
+        [Display(Name="Ciudad")]
+        public string Name { get; set; }    
+    }
+}
